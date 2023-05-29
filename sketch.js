28b7653636ab18044534186b403746abf09bce05
10832638 <134923721+10832638@users.noreply.github.com>
2023-05-29 17:39:55 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://10832638.github.io/frog/model.json';
 
 
 function preload() {
